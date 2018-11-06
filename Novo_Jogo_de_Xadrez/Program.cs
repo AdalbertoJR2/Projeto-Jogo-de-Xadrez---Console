30f4d40fc84c7326c9a422bee544b803d2868ee3
Adalberto JR <adalbertopsjunior01@gmail.com>
2018-11-06 19:32:21 -0200

    Melhorando o metodo de imprimir tabuleiro.

--- Novo_Jogo_de_Xadrez/Program.cs
+++ Novo_Jogo_de_Xadrez/Program.cs
@@ -13,7 +13,10 @@ namespace Novo_Jogo_de_Xadrez
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(2, 3));
+
+            tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(5, 3));
 
 
                 Tela.imprimirTabuleiro(tab);
