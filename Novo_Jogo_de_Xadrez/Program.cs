7f1ed980e0ea88c2ea6498cb26ea831d0a686a5c
Adalberto JR <adalbertopsjunior01@gmail.com>
2018-11-06 18:23:15 -0200

    Implementado metodos e propriedades.

--- Novo_Jogo_de_Xadrez/Program.cs
+++ Novo_Jogo_de_Xadrez/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using tabuleiro;
-
+using xadrez;
 
 namespace Novo_Jogo_de_Xadrez
 {
@@ -8,7 +8,13 @@ namespace Novo_Jogo_de_Xadrez
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
 
         }
     }
